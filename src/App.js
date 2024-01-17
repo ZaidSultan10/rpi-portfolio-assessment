27ecf3190646225c9e54c5db8103fea9b101f297
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-18 00:24:43 +0400

    wireframe of header and login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      portfollio website
-    </div>
+    <main className="App">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <HomePage/> } />
+          <Route path='/login' element = { <LoginPage /> } />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
